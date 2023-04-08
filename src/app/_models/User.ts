65c790e1afec7c9d485215ab41906e492941e662
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-04-08 08:45:41 +0200

    added user model

---
+++ src/app/_models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    _id?: string;
+    firstName?: string;
+    lastName?: string;
+    userName?: string;
+    email: string;
+    password: string;
+    userRole?: string;
+    userImage?: string;
+    token?: string;
+};
