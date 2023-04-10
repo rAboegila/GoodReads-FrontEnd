73b86db3930cf9a533094776356570c03f7a33dc
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-04-11 00:57:20 +0200

    confirm password field with validations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,6 +5,21 @@ import { first } from 'rxjs';
 import { User } from 'src/app/_models/User';
 import { UserService } from 'src/app/_services/user.service';
 
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class CustomValidators {
+  static MatchValidator(source: string, target: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const sourceCtrl = control.get(source);
+      const targetCtrl = control.get(target);
+
+      return sourceCtrl && targetCtrl && sourceCtrl.value !== targetCtrl.value
+        ? { mismatch: true }
+        : null;
+    };
+  }
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -21,10 +36,13 @@ export class RegisterComponent {
     this.myForm = new FormGroup({
       firstName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
       lastName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-      username: new FormControl(null, [Validators.minLength(3), Validators.maxLength(20)]),
+      username: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-    });
+      confirmPassword: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    },
+      [CustomValidators.MatchValidator('password', 'confirmPassword')]
+    );
   }
 
   submitForm() {
@@ -34,6 +52,7 @@ export class RegisterComponent {
     formData.append('username', this.myForm.get('username')?.value);
     formData.append('email', this.myForm.get('email')?.value);
     formData.append('password', this.myForm.get('password')?.value);
+    formData.append('confirmPassword', this.myForm.get('confirmPassword')?.value);
     formData.append('image', this.image);
 
     this._userService.register(formData)
@@ -54,4 +73,12 @@ export class RegisterComponent {
       this.image = file;
     }
   }
+
+  get passwordMatchError() {
+    return (
+      this.myForm.getError('mismatch') &&
+      this.myForm.get('confirmPassword')?.touched
+    );
+  }
+
 }
