4dc9105a11d62e716eec6fc177b38d2a47e57c34
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-04-11 11:44:26 +0200

    Added basic template with the created Rating Component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent {
 
-  constructor(private _userService: UserService) {
+  constructor(private _userService: UserService, private snackBar: MatSnackBar) {
     console.log("ProfileComponent: ", this._userService.getProfile());
   }
 
