6d164c5a0e95033cd3a407c60aabd618dd3691a7
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-04-13 21:18:26 +0200

    MOdified Book and User Interfaces + Added Library Interface + added BookShelf Enum + added script in index.html

--- src/app/_models/Book.ts
+++ src/app/_models/Book.ts
@@ -4,20 +4,17 @@ import { Category } from "./Category";
 export interface Book {
   _id?: string;
   name: string;
-  category: Category['_id'];
-  author: Author;
-  image?: string;
-  avgRating?: number;
-  reviews?: string[];
-}
-
-export  interface BookDetiles{
-  _id?: string;
-  name: string;
   category: Category;
   author: Author;
   image?: string;
   avgRating?: number;
   reviews?: string[];
+  rating?: number;
+  shelve?: BookShelf;
+}
 
+export enum BookShelf {
+  READING = 'READING',
+  READ = 'READ',
+  WANT_TO_READ = 'WANT_TO_READ',
 }
