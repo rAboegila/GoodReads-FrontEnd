f665aa450cb609f85bcdcacfb2fa2a39c591078f
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-04-16 00:38:21 +0200

    edit Author

--- src/app/edit-author/edit-author.component.ts
+++ src/app/edit-author/edit-author.component.ts
@@ -78,6 +78,7 @@ export class EditAuthorComponent {
   //   });
   // }
 
+
   createForm() {
     this.authorFormEdit = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
@@ -93,6 +94,7 @@ export class EditAuthorComponent {
     }
   }
 
+
   onupdate() {
     const author: Author = {
       firstName: this.authorFormEdit!.value.firstName,
@@ -104,7 +106,20 @@ export class EditAuthorComponent {
     const formData = new FormData();
     formData.append('firstName', author.firstName);
     formData.append('lastName', author.lastName);
-    formData.append('dob', author.dob.toString());
+    // formData.append('dob', author.dob.toString());
+    if (this.authorFormEdit) {
+      const authorData = this.authorFormEdit.value;
+      authorData.id = this.Author._id;
+      // const formData = new FormData();
+      for (const key of Object.keys(authorData)) {
+        if (key !== 'image') {
+          formData.append(`Author.${key}`, authorData[key]);
+        } else {
+          formData.append(key, authorData[key]);
+        }
+      }
+      }
+
     if (this.selectedImage) {
       formData.append('image', this.selectedImage, this.selectedImage.name);
     }
