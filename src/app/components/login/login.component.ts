b077043bcc205dd33d4e6823596a44331cdeda29
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-04-17 01:12:29 +0200

    edit valadtion password and username

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,6 +13,8 @@ import { UserService } from 'src/app/_services/user.service';
 export class LoginComponent {
 
   users!: User[];
+  errorMessage!:string;
+
 
   constructor(private _userService: UserService, private _router: Router) {
   }
@@ -22,10 +24,12 @@ export class LoginComponent {
       .subscribe({
         next: () => {
           form.reset();
+          this.errorMessage = '';
           this._router.navigate(['/']);
           // this._toast.success({ detail: "You have successfully logged in", summary: "Login Success", duration: 5000 });
         },
         error: res => {
+          this.errorMessage = 'invalid data';
           if (res.error.errors) {
             res.error.errors.forEach((error: any) => {
               // this._toast.error({ detail: "Failed To Login!", summary: (error.msg ? error.msg : error).split(',')[1], duration: 5000 });
